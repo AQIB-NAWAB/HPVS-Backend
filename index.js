7b43075ebfb7f266521cfc29855bce73dabf9ff2
Aqib Nawab <aqibnawab1100@gmail.com>
2023-11-21 18:00:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path")
 require("dotenv").config({ path: "./config/.env" });
 app.use(cors(
     {
-        origin:"*",
+        origin:["https://hpvs.vercel.app"],
         withCredentials:true,
     }
 
